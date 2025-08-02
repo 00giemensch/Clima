a3ac5c7ae49df630247a348befe5004b51c5d212
00giemensch <75619006+00giemensch@users.noreply.github.com>
2025-08-02 23:37:37 +0400

    start project

--- Clima/SceneDelegate.swift
+++ Clima/SceneDelegate.swift
@@ -2,8 +2,7 @@
 //  SceneDelegate.swift
 //  Clima
 //
-//  Created by Angela Yu on 01/09/2019.
-//  Copyright © 2019 App Brewery. All rights reserved.
+//  Created by Ilnur on 02.08.2025.
 //
 
 import UIKit
