ad467c21faa398d2d75035bc7189de8fc9827e60
00giemensch <75619006+00giemensch@users.noreply.github.com>
2025-08-03 14:07:58 +0400

    добавил методы для UITextField

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -7,17 +7,49 @@
 
 import UIKit
 
-class WeatherViewController: UIViewController {
-
+class WeatherViewController: UIViewController, UITextFieldDelegate {
+    
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var searchTextField: UITextField!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        searchTextField.delegate = self // берет на себя управление событиями текстового поля searchTextField
+
     }
-
-
+    
+    
+    // кнопка поиск - по нажатию клавиатура скрывается, и поле завершает редактирование
+    @IBAction func searchPressed(_ sender: UIButton) {
+        searchTextField.endEditing(true)
+    }
+    
+    // разрешить клавиатуре скрыться после нажатия return(go)
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchTextField.endEditing(true)
+        return true
+    }
+    
+    // метод вызывается перед тем, как клавиатура должна скрыться
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if searchTextField.text != "" {
+            return true // если текст не пустой, то клавиатура скрывается
+        } else {
+            searchTextField.placeholder = "Type something"
+            return false // если пустой, то клавиатура не скрывается
+        }
+    }
+    
+    // cрабатывает, когда редактирование реально закончилось — после нажатия “Поиск”, “Return”, или тапа вне поля
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        // Use searchTextField.text to get the weather for that city
+        searchTextField.text = ""
+    }
+    
+    
 }
 
